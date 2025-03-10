be736ad81de1838948b8feb7f5f0fc05fa849720
Jay Turner <jaynicholasturner@gmail.com>
2025-03-10 09:24:36 +0000

    Last will for uptime

--- mqttdevice/plugins/uptime.py
+++ mqttdevice/plugins/uptime.py
@@ -1,3 +1,5 @@
+import json
+import aiomqtt
 from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import UnitOfTime
 
@@ -15,6 +17,14 @@ class Plugin(Sensor):
         with open("/proc/uptime") as f:
             return float(f.read().split()[0])
 
+    async def on_connect(self, client: aiomqtt.Client):
+        await super().on_connect(client)
+        device_class = (self.device_class.value if isinstance(self.device_class, SensorDeviceClass) else self.device_class) or "state"
+        self.will_set(
+            self.state_topic,
+            json.dumps({device_class: 0}),
+            retain=True,
+        )
 
 def setup(device: Device, config: PluginConfig):
     Plugin(device, config)
